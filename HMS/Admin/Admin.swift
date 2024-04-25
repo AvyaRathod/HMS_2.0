63cce28308ecfb8a5b964f94ce180d46f6ceea70
KP VISHNU SAI <kpvishnusai1111@gmail.com>
2024-04-25 09:44:17 +0530

    admin

--- HMS/Admin/Admin.swift
+++ HMS/Admin/Admin.swift
@@ -9,9 +9,45 @@ import SwiftUI
 
 struct Admin: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            Text("Home")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            Text("Record")
+                .tabItem {
+                    Image(systemName: "book")
+                    Text("Record")
+                }
+            StaffInfoView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person.3.fill")
+                            .foregroundColor(.red)
+                            .frame(width:100, height: 100)
+                            .clipShape(Circle())
+                            .background(.red)
+                        Text("Staff Info")
+                    }
+                }
+            Text("Alert")
+                .tabItem {
+                    Image(systemName: "light.beacon.max.fill")
+                    Text("Alert")
+                }
+            Text("Health Events")
+                .tabItem {
+                    Image(systemName: "heart.square.fill")
+                    Text("Health Events")
+                }
+        }
     }
 }
+
+
+
+
 
 #Preview {
     Admin()
