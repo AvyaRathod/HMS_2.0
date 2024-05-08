96add185ab803dc731205a9200740f3a36176240
Arul Gupta <arul17gupta@gmail.com>
2024-05-08 15:20:14 +0530

    sosAdmin patch

--- HMS/Admin/SOSAdmin.swift
+++ HMS/Admin/SOSAdmin.swift
@@ -14,14 +14,13 @@ import FirebaseFirestore
 struct SOSAdmin: View {
     @StateObject private var emergencyViewModel = EmergencyViewModel()
     @State private var timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
-
     
     var body: some View {
         ScrollView{
             VStack(spacing: 20) {
                 
                 ForEach(emergencyViewModel.emergencies, id: \.self) { emergency in
-                    LocationCard(name: emergency.patientId,
+                    LocationCard(patid: emergency.patientId,
                                  latitude: emergency.latitude,
                                  longitude: emergency.longitude,
                                  dateTime: formattedDate(from: emergency.timeStamp))
@@ -30,6 +29,7 @@ struct SOSAdmin: View {
             }
             .padding()
             .onAppear {
+
                 emergencyViewModel.getAllEmergencies()
             }
             .onReceive(timer) { _ in
@@ -47,42 +47,54 @@ struct SOSAdmin: View {
     
 }
 struct LocationCard: View {
-    let name: String
+    @State private var pName: String = "Loading..."
+    let patid: String
     let latitude: String
     let longitude: String
     let dateTime: String
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text(name)
-                .font(.title)
-            Text("Location:")
-                .font(.headline)
-            Text("Latitude: \(latitude), Longitude: \(longitude)")
-                .font(.subheadline)
-            Text("Date & Time: \(dateTime)")
-                .font(.subheadline)
-            HStack {
-                Spacer()
-                Button(action: {
-                    openMaps(latitude: latitude, longitude: longitude)
-                }) {
-                    Text("Locate")
-                        .foregroundStyle(Color.white)
+        ZStack{
+            VStack(alignment: .leading, spacing: 10) {
+                Text(pName)
+                    .font(.title)
+                Text("Location:")
+                    .font(.title2)
+                Text("Latitude: ")
+                    + Text("\(latitude)").bold()
+                    .font(.title3)
+                Text("Longitude: ")
+                    + Text("\(longitude)").bold()
+                    .font(.title3)
+                Text("Date & Time:")
+                    + Text("\(dateTime)").bold()
+                    .font(.title3)
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        openMaps(latitude: latitude, longitude: longitude)
+                    }) {
+                        Text("Locate")
+                            .foregroundStyle(Color.white)
+                    }
+                    .font(.title3)
+                    .frame(width:100,height: 50)
+                    .background(Color.customBlue)
+                    .cornerRadius(11)
                 }
-               
-                .fontWeight(.heavy)
-                .font(.title3)
-                .frame(width:300)
                 .padding()
-                .foregroundColor(.white)
-                .background(Color(Color.black))
-                .cornerRadius(40)
-        }.padding()
+            }
+            .padding()
+            .background(Color.white)
+            RoundedRectangle(cornerRadius: 11)
+                .strokeBorder(Color.black, lineWidth: 1)
+                .shadow(radius: 4)
         }
-//        .padding()
-        .background(Color.white)
-        .border(Color.black)
+        .onAppear{
+            fetchPName()
+        }
+
+
     }
 
     func openMaps(latitude: String, longitude: String) {
@@ -91,7 +103,29 @@ struct LocationCard: View {
         }
         
     }
+    
+    private func fetchPName() {
+        let db = Firestore.firestore()
+        let documentID = patid
+        db.collection("Patients").document(documentID).getDocument { (document, error) in
+            if let document = document, document.exists {
+                let data = document.data()
+                if let patientName = data?["name"] as? String {
+                    print("Patient Name: \(patientName)")
+                    pName=patientName
+                } else {
+                    print("Patient name not found in the document.")
+                }
+            } else {
+                print("Document does not exist or error: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+    }
+    
 }
+
+
+
 
 
 struct SOSAdmin_Previews: PreviewProvider {
